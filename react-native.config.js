35bef17e37f42702011bcb99cbbedefbf7e877e6
hoonjoo-park <hoonjoo94@naver.com>
2025-05-28 19:12:10 +0900

    refactor: react-native.config 수정

--- react-native.config.js
+++ react-native.config.js
@@ -2,6 +2,8 @@ module.exports = {
   dependency: {
     platforms: {
       android: {
+        sourceDir: './android/app',
+        packageImportPath: 'import com.microsoft.codepush.react.CodePush;',
         packageInstance:
           'CodePush.getInstance(getResources().getString(R.string.CodePushDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)',
       },
